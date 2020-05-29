226a58da2c1f3c0c66a50e9c15cae38f7d848612
gzonelee <gzonelee@gmail.com>
2020-05-29 09:21:04 +0900

    Initial Commit

---
+++ DateFormatting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DateFormatting
+//
+//  Created by chlee on 2020/05/29.
+//  Copyright © 2020 chlee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
