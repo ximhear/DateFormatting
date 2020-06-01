6c1b3c946be8a1381f313523070a60eddf06f269
gzonelee <gzonelee@gmail.com>
2020-06-01 10:51:56 +0900

    first

---
+++ DateFormatting/DateFormatView.swift
@@ -0,0 +1,21 @@
+//
+//  DateFormatView.swift
+//  DateFormatting
+//
+//  Created by chlee on 2020/05/29.
+//  Copyright © 2020 chlee. All rights reserved.
+//
+
+import SwiftUI
+
+struct DateFormatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DateFormatView_Previews: PreviewProvider {
+    static var previews: some View {
+        DateFormatView()
+    }
+}
---
+++ DateManager.swift
@@ -0,0 +1,9 @@
+//
+//  DateManager.swift
+//  DateFormatting
+//
+//  Created by chlee on 2020/05/29.
+//  Copyright © 2020 chlee. All rights reserved.
+//
+
+import Foundation
